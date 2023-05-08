8e48c5053dfa5762bfbdc87a1640ec9683a787b6
uhee <uhee@mz.co.kr>
2023-05-08 18:17:57 +0900

    앱 만들기 시작..

--- my-react-practice/src/App.js
+++ my-react-practice/src/App.js
@@ -2,30 +2,26 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  // console.log("always render");
-  useEffect(() => {
-    console.log("API render"); //딱 한번만 실행됨 (배열 빈값 = 지켜볼게 없다)
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("검색합니다", keyword);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos]= useState([]);
+  const onChange = (event)=> setTodo(event.target.value);
+  const onSubmit = (event)=> {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
     }
-  }, [keyword]);
-
+    
+    setTodo("");
+    //변경된 todo 를 현재 배열에 넣는 구문 ...+배열명
+    setTodos((currentArray) => [toDo , ...currentArray]);
+  };
   return (
     <div className="App">
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="검색.."
-      />
-      <h1 className={styles.title}>클릭한 횟수 :: {counter}</h1>
-      <button onClick={onClick}>버튼임당</button>
+      <h1>My Todos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} onChange={onChange} type="text" placeholder="내용을 입력해주삼"/>
+        <button>Add</button>
+      </form>
     </div>
   );
 }
